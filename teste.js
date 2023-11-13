4a05acb639b3111c44c80c03dff092a05aba0fc1
Igor Santos <142364693+Igorsantos021@users.noreply.github.com>
2023-11-13 23:20:39 +0000

    Laço enquanto - while

---
+++ teste.js
@@ -0,0 +1,16 @@
+let contador = 1
+while(contador <= 10){
+    console.log(contador)
+    contador++
+}
+
+while(contador >= 1){
+    console.log(contador)
+    contador--
+}
+    
+let i = 1
+while(i <= 1){
+    console.log("Botafogo Campeão")
+    i++
+}
