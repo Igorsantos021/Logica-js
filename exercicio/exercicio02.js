8febd781fb99aed096a330e347e7c719dcd99794
Igor Santos <142364693+Igorsantos021@users.noreply.github.com>
2023-11-14 22:43:05 +0000

    Atividades feitas

--- exercicio/exercicio02.js
+++ exercicio/exercicio02.js
@@ -1,8 +1,9 @@
 /*Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.*/
 
-let i=1
-let numeros
-while(i<=20){
-    
+let i =1
+while(i <= 50){
+    if(i%2 != 0){
+    console.log(i)
+    }
+i++
 }
-console.log(numeros)
